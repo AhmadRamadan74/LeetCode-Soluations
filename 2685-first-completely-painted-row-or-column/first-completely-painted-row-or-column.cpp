a47d0657285b12ba01d7fbea5c4a1ca785692df8
Ahmad Ramadan <113770965+AhmadRamadan74@users.noreply.github.com>
2025-01-20 20:49:31 +0200

    Time: 0 ms (100.00%) | Memory: 129.6 MB (100.00%) - LeetSync

---
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int n = int(mat.size());
+        int m = int(mat[0].size());
+        int size = n * m;
+
+        pair< int , int > mapping[ n * m + 1 ];
+        int row[n + 1], col[m + 1];
+        fill(row, row + n + 1, 0);
+        fill(col, col + m + 1, 0);
+        
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                mapping[ mat[i][j] ] = {i,j};
+            }
+        }
+
+        for (int i = 0; i < size; ++i) {
+
+            auto [y, x] = mapping[ arr[i] ];
+            row[ y ] ++;
+            col[ x ] ++;
+
+
+            if( (row[y] == m) or (col[x] == n) )
+                return i;
+        }
+        return -1;
+    }
+};
