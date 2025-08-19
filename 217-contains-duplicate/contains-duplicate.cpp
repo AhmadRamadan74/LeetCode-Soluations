f464cfce1c9c097d708e56baf7f4ed9d35c46d21
Ahmad Ramadan <113770965+AhmadRamadan74@users.noreply.github.com>
2025-08-19 21:06:24 +0300

    Time: 134 ms (5.18%) | Memory: 94.9 MB (5.17%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int,int>mp;
-        for(auto i : nums){
-            mp[i]++;
-            if(mp[i] > 1) return true;
+        sort(nums.begin() , nums.end());
+        map<int , int>freq;
+        for(auto i : nums) freq[i]++;
+        for(auto  i : freq) {
+            if(i.second > 1) return true;
         }
         return false;
     }
