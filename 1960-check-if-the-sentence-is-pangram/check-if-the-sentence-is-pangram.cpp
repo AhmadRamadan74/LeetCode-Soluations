e347f95a9f4d989cc796ead527486ef0d350acba
Ahmad Ramadan <113770965+AhmadRamadan74@users.noreply.github.com>
2025-11-09 17:36:46 +0200

    Time: 0 ms (100.00%) | Memory: 8.7 MB (96.28%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkIfPangram(string s) {
+        vector<bool> visit(26, false);
+        for(int i = 0; i < s.size(); i++)
+            visit[s[i]-'a'] = true;
+        for(int i = 0; i < 26; i++)
+            if(!visit[i])
+                return false;
+        return true;
+    }
+};
