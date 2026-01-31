ea7b49dd5cc52b8cf18fab1c8743b99c02c8e979
Ahmad Ramadan <113770965+AhmadRamadan74@users.noreply.github.com>
2026-01-31 21:05:33 +0200

    Time: 0 ms (100.00%) | Memory: 31.4 MB (7.51%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -3,7 +3,7 @@ public:
     int search(vector<int>& nums, int target) {
         int l = 0 , r = nums.size() - 1;
         while(l <= r) {
-            int mid = floor((l + r) / 2);
+            int mid = (l + r) / 2;
             if(nums[mid] == target) return mid;
             else if(nums[mid] > target) r = mid - 1;
             else l = mid + 1;
