4a5847c09ff846f45aeacc92d0235e590020cda3
Ahmad Ramadan <113770965+AhmadRamadan74@users.noreply.github.com>
2026-09-02 14:43:41 +0300

    Time: 0 ms (100.00%) | Memory: 21.8 MB (38.72%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int missingNumber(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        bool flag = false;
-        int ans;
-        for(int i = 0; i < arr.size(); i++) {
-            if(arr[i] != i) {
-                ans = i;
-                flag = true;
-                break;
-            }
-        }
-        return ((flag) ? ans : arr.back() + 1);
+        int n = arr.size();
+        int all = n * (n + 1) / 2;
+        int sum = 0;
+        for(auto i: arr)sum += i;
+        return all - sum;
     }
 };
