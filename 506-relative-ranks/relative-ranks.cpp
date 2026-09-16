92d885eed71d792b3730527550c0a166ff211edf
Ahmad Ramadan <113770965+AhmadRamadan74@users.noreply.github.com>
2026-09-16 14:26:19 +0300

    Time: 3 ms (79.63%) | Memory: 17.8 MB (13.12%) - LeetSync

--- 506-relative-ranks/relative-ranks.cpp
+++ 506-relative-ranks/relative-ranks.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
-        int st = 4;
-        vector<int>arr = score;
-        vector<string>s = {"Gold Medal", "Silver Medal", "Bronze Medal"};
-        sort(arr.begin(), arr.end()), reverse(arr.begin(), arr.end());
-        map<int ,string>mp;
-        int cnt = 3, it = 0;
-        for(auto i: arr) {
-            if(cnt > 0) {
-                mp[i]  = s[it]; // 0 .1 . 2
-                cnt--,it++;
-            }
-            else {
-                mp[i] = to_string(st);
-                st++;
-            }
+        vector<pair<int, int>>vp;
+        int n = score.size();
+        for(int i = 0; i < n; i++) {
+            vp.emplace_back(score[i], i);
         }
-        vector<string>ans;
-        for(auto i: score) {
-            ans.push_back(mp[i]);
+        sort(vp.rbegin(), vp.rend());
+        vector<string>ans(n);
+        for(int i = 0; i < n; i++) {
+            int index = vp[i].second;
+            if(i == 0) ans[index] = "Gold Medal";
+            else if(i == 1) ans[index]= "Silver Medal";
+            else if(i == 2) ans[index]= "Bronze Medal";
+            else {
+                ans[index] = to_string(i + 1);
+            }
         }
         return ans;
     }
